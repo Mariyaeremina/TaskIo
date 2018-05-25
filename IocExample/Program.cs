c13d6187ede7f150585b4697573155d5fcae9e96
Mariyaeremina <Mariyaeremina@outlook.com>
2018-05-26 00:24:39 +0500

    initial commit

---
+++ IocExample/Program.cs
@@ -0,0 +1,16 @@
+﻿using IocExample.Classes;
+
+namespace IocExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = new ConsoleLogger();
+            var sqlConnectionFactory = new SqlConnectionFactory("SQL Connection", logger);
+            var createUserHandler = new CreateUserHandler(new UserService(new QueryExecutor(sqlConnectionFactory), new CommandExecutor(sqlConnectionFactory), new CacheService(logger, new RestClient("API KEY"))), logger);
+
+            createUserHandler.Handle();
+        }
+    }
+}
